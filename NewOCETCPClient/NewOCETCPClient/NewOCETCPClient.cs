df5cabb08a988afea3d39a315b185b3c82b2faa6
ComarPers922 <18982127626@163.com>
2016-03-05 23:24:59 +0800

    Code...

---
+++ NewOCETCPClient/NewOCETCPClient/NewOCETCPClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace NewOCETCPClient
+{
+    public class MyNewOCETCPClient
+    {
+        private TcpClient tcpclient;
+        private Thread receiver;
+        private NetworkStream ns;
+        public delegate void MessageArrived(object sender, byte[] message);
+        public event MessageArrived MessageArrivedHandler;
+        public MyNewOCETCPClient()
+        {
+            tcpclient = new TcpClient();
+        }
+        public MyNewOCETCPClient(TcpClient tcpclient)
+        {
+            this.tcpclient = tcpclient;
+            Initialize();
+        }
+        private void Initialize()
+        {
+            ns = tcpclient.GetStream();
+            receiver = new Thread(delegate()
+            {
+                while (true)
+                {
+                    if (tcpclient.Available < 1)
+                    {
+                        continue;
+                    }
+                    byte[] b = new byte[tcpclient.Available];
+                    ns.Read(b, 0, b.Length);
+                    if (this.MessageArrivedHandler != null)
+                    {
+                        MessageArrivedHandler(this, b);
+                    }
+                }
+            });
+            receiver.IsBackground = true;
+            receiver.Start();
+        }
+        public void Connect(string addr, int port)
+        {
+            this.tcpclient.Connect(IPAddress.Parse(addr), port);
+            Initialize();
+        }
+        public void Send(byte[] message)
+        {
+            ns.Write(message, 0, message.Length);
+        }
+        public void SendString(string message)
+        {
+            SendString(message,Encoding.Default);
+        }
+        public void SendString(string message , Encoding encoding)
+        {
+            this.Send(encoding.GetBytes(message));
+        }
+        public void Close()
+        {
+            receiver.Abort();
+            ns.Dispose();
+            tcpclient.Close();
+        }
+    }
+}
---
+++ NewOCETCPListener/NewOCETCPListener/MyNewOCETCPListener.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NewOCETCPListener
+{
+    public class MyNewOCETCPListener
+    {
+        private TcpListener tcplistener;
+        private Thread listening;
+
+        public delegate void TCPAcceptRequestArrived(object sender,TcpClient tcpclient);
+        public event TCPAcceptRequestArrived TCPAcceptRequestArrivedHandler;
+
+        private void InitializeThread()
+        {
+            tcplistener.Start();
+            listening = new Thread(delegate()
+                {
+                    var temp = tcplistener.AcceptTcpClient();
+                    while (true)
+                    {
+                        if (TCPAcceptRequestArrivedHandler != null)
+                        {
+                            TCPAcceptRequestArrivedHandler(this, temp);
+                        }
+                    }
+                });
+            listening.IsBackground = true;
+            listening.Start();
+        }        
+        
+        [Obsolete("不推荐使用该方法")]
+        public MyNewOCETCPListener(int port)
+        {
+            this.tcplistener = new TcpListener(port);
+        }
+        public MyNewOCETCPListener(IPAddress addr,int port)
+        {
+            this.tcplistener = new TcpListener(addr,port);
+        }
+        public void Start()
+        {
+            this.InitializeThread();
+        }
+        public void Close()
+        {
+            this.tcplistener.Stop();
+            this.listening.Abort();
+        }
+    }
+}
