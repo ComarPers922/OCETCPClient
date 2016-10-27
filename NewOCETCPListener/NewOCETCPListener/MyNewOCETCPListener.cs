f057f5d6820d75ddb82724fa585d64ac665a1043
ComarPers CMXXII Leo <18982127626@163.com>
2016-10-27 17:42:35 -0400

    Added some comments

--- NewOCETCPListener/NewOCETCPListener/MyNewOCETCPListener.cs
+++ NewOCETCPListener/NewOCETCPListener/MyNewOCETCPListener.cs
@@ -12,9 +12,16 @@ namespace NewOCETCPListener
         private TcpListener tcplistener;
         private Thread listening;
 
+        /// <summary>
+        /// Handling new connecting requests
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="tcpclient">Incoming Client Request</param>
         public delegate void TCPAcceptRequestArrived(object sender,TcpClient tcpclient);
         public event TCPAcceptRequestArrived TCPAcceptRequestArrivedHandler;
-
+        /// <summary>
+        /// Prepare the listener
+        /// </summary>
         private void InitializeThread()
         {
             tcplistener.Start();
@@ -32,20 +39,34 @@ namespace NewOCETCPListener
             listening.IsBackground = true;
             listening.Start();
         }        
-        
+        /// <summary>
+        /// Initialize a listener using port
+        /// </summary>
+        /// <param name="port"></param>
         [Obsolete("This method is not recommanded!")]
         public MyNewOCETCPListener(int port)
         {
             this.tcplistener = new TcpListener(port);
         }
+        /// <summary>
+        /// Initianlize a listener using ipaddress and port
+        /// </summary>
+        /// <param name="addr">IPAddress</param>
+        /// <param name="port">Port</param>
         public MyNewOCETCPListener(IPAddress addr,int port)
         {
             this.tcplistener = new TcpListener(addr,port);
         }
+        /// <summary>
+        /// Start the listener
+        /// </summary>
         public void Start()
         {
             this.InitializeThread();
         }
+        /// <summary>
+        /// Close the listener
+        /// </summary>
         public void Close()
         {
             this.tcplistener.Stop();
