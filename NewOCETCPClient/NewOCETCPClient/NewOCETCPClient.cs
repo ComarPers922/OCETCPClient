2e6394cdf5c8e170635d8e7efc1ccae1009ae330
ComarPers CMXXII Leo <18982127626@163.com>
2016-10-27 17:49:27 -0400

    Added some comments

--- NewOCETCPClient/NewOCETCPClient/NewOCETCPClient.cs
+++ NewOCETCPClient/NewOCETCPClient/NewOCETCPClient.cs
@@ -14,17 +14,32 @@ namespace NewOCETCPClient
         private TcpClient tcpclient;
         private Thread receiver;
         private NetworkStream ns;
+        /// <summary>
+        /// Handle incoming messgae
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="message">Incoming message</param>
         public delegate void MessageArrived(object sender, byte[] message);
         public event MessageArrived MessageArrivedHandler;
+        /// <summary>
+        /// Create a new TCPClient
+        /// </summary>
         public MyNewOCETCPClient()
         {
             tcpclient = new TcpClient();
         }
+        /// <summary>
+        /// Create a new TCPClient using copy from other instance
+        /// </summary>
+        /// <param name="tcpclient">Other instance</param>
         public MyNewOCETCPClient(TcpClient tcpclient)
         {
             this.tcpclient = tcpclient;
             Initialize();
         }
+        /// <summary>
+        /// Initialize new instance using
+        /// </summary>
         private void Initialize()
         {
             ns = tcpclient.GetStream();
@@ -47,23 +62,44 @@ namespace NewOCETCPClient
             receiver.IsBackground = true;
             receiver.Start();
         }
+        /// <summary>
+        /// Connect to Server
+        /// </summary>
+        /// <param name="addr">IPAddress</param>
+        /// <param name="port">Port</param>
         public void Connect(string addr, int port)
         {
             this.tcpclient.Connect(IPAddress.Parse(addr), port);
             Initialize();
         }
+        /// <summary>
+        /// Send message using bytes
+        /// </summary>
+        /// <param name="message">Message attempted to send</param>
         public void Send(byte[] message)
         {
             ns.Write(message, 0, message.Length);
         }
+        /// <summary>
+        /// Send string message using Default encoding directly.
+        /// </summary>
+        /// <param name="message">Message attempted to send</param>
         public void SendString(string message)
         {
             SendString(message,Encoding.Default);
         }
+        /// <summary>
+        /// Send string message directly
+        /// </summary>
+        /// <param name="message">Message attempted to send</param>
+        /// <param name="encoding">Encoding</param>
         public void SendString(string message , Encoding encoding)
         {
             this.Send(encoding.GetBytes(message));
         }
+        /// <summary>
+        /// Close this client
+        /// </summary>
         public void Close()
         {
             receiver.Abort();
